fdac2a30269b2da7ab2eee337e9e2aa99916559e
MuhammadGalimov <kakasi.muham@gmail.com>
2021-01-20 04:47:30 +0500

    added customuser

--- vision_test/settings.py
+++ vision_test/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users.apps.UsersConfig',
     'app',
+    'account',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
